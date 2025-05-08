a6a5db695843e517a07a12ce7f4ee331830f5c78
Gregory-Swan <anonymous.swan0@gmail.com>
2025-05-08 18:25:11 +0300

    Tests

---
+++ vigenere.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+namespace vigenere.Tests
+{
+    public class VigenereTests
+    {
+        [Fact]
+        public void VigenereTest()
+        {
+            string message = "Привет! Hello!";
+            string key = "абвabc";
+            string result = Crypt.VigenereEncrypt(message, key);
+            Assert.Equal("Ртлгжх! Igomq!", result);
+            Assert.Equal(message, Crypt.VigenereDecrypt(result, key));
+        }
+        [Fact]
+        public void CaesarTest()
+        {
+            string message = "Привет! Hello!";
+            int shift = 3;
+            string result = Crypt.CaesarEncrypt(message, shift);
+            Assert.Equal("Тулеих! Khoor!", result);
+            Assert.Equal(message, Crypt.CaesarDecrypt(result, shift));
+        }
+        [Fact]
+        public void AtbashTest()
+        {
+            string message = "Привет! Hello!";
+            string result = Crypt.AtbashTransform(message);
+            Assert.Equal("Рпчэън! Svool!", result);
+            Assert.Equal(message, Crypt.AtbashTransform(result));
+        }
+    }
+}
